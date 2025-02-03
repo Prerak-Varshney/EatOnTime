731e08acfa1df67853b82bc65a6a0bd56613e3ea
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-02-03 14:46:42 +0530

    Added admin dashboard

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -1,21 +1,28 @@
-const Navbar = () => {
-   return (
+'use client';
+import { useState } from 'react';
+
+const DashboardNavbar = () => {
+    const [selectedNav, setSelectedNav] = useState('Dashboard');
+    return (
        <>
            <div className={`w-80 h-screen bg-slate-900 flex flex-col items-center justify-start`}>
-               <div className={`w-full h-40 flex flex-col items-center justify-center`}>
+               <div className={`w-full h-40 flex flex-col items-center justify-center border-b border-slate-700`}>
                    <span className={`font-bold text-2xl text-white`}>Restaurant Name</span>
                </div>
 
-               <nav className={`w-11/12 text-white flex flex-col items-center justify-center`}>
+               <nav className={`w-11/12 text-white flex flex-col items-center justify-center mt-10`}>
                    {
                        ['Dashboard', 'Items', 'Orders', 'Deliveries', "Employees"].map((item) =>
-                           <span className={`w-full h-14 flex items-center justify-center hover:bg-slate-800 font-medium transition-all duration-300`} key={item}>{item}</span>
+                           <span
+                               className={`w-full h-14 flex items-center justify-center border border-transparent hover:bg-slate-800 hover:border-slate-700 font-medium transition-all duration-300 ${selectedNav === item ? 'bg-slate-800 hover:border-transparent' : ''}`}
+                               key={item}
+                               onClick={() => setSelectedNav(item)}
+                           >{item}</span>
                        )
                    }
                </nav>
            </div>
        </>
-   )
+    )
 }
-
-export default Navbar;
+export default DashboardNavbar;
